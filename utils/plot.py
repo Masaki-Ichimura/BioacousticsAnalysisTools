789974bec2a9e315eb7db6efbccd5b71a60b616e
Masaki-Ichimura <m.ichimura.lab@gmail.coom>
2022-05-25 11:56:48 +0900

    Add displaying audio signal

--- utils/plot.py
+++ utils/plot.py
@@ -4,7 +4,7 @@ import librosa.display
 import matplotlib.pyplot as plt
 
 
-def show_spec(waveform, fs, n_fft=2048,**kw):
+def show_spec(waveform, fs, n_fft=2048, **kw):
     waveform = waveform if waveform.ndim == 1 else waveform.mean(0)
     amp_np = torch.stft(
         waveform, n_fft=n_fft, return_complex=True
@@ -18,7 +18,14 @@ def show_spec(waveform, fs, n_fft=2048,**kw):
     )
     return plt_obj
 
-def show_wav(waveform):
+def show_wav(waveform, fs, ax=None):
     waveform = waveform if waveform.ndim == 1 else waveform.mean(0)
-    plt_obj = plt.plot(waveform)
-    return plt_obj
+    t_len = waveform.shape[0]
+    t = torch.linspace(1/fs, t_len/fs, t_len)
+
+    if ax:
+        ax.plot(t, waveform)
+        return ax
+    else:
+        plt_obj = plt.plot(t, waveform)
+        return plt_obj
