Fix smooth moving average function

app/gui/config_container.py
@@ -1,5 +1,9 @@
+import torch
+
 from kivy.lang import Builder
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
+from kivymd.uix.menu import MDDropdownMenu
 
 from app.gui.widgets.container import Container
 
@@ -10,9 +14,47 @@ class ConfigContainer(Container):
     def on_kv_post(self, *arg, **kwargs):
         self.ids.nav_drawer.set_state('open')
 
+        self.stft_window_menu = MDDropdownMenu(
+            caller=self.ids.stft_window,
+            hor_growth='right',
+            width_mult=4,
+            items=[
+                dict(
+                    viewclass='OneLineListItem',
+                    text=k,
+                    height=dp(54),
+                    on_release=lambda x=k: self.set_stft_window(x)
+                )
+                for k in ['bartlett', 'blackman', 'hamming', 'hann', 'kaiser']
+            ]
+        )
+
+    def set_stft_window(self, window):
+        self.ids.stft_window.set_text(self.ids.stft_window, window)
+        self.stft_window_menu.dismiss()
+
     def get_stft_args(self):
+        n_fft = int(self.ids.stft_n_fft.text)
+
+        hop_length = int(self.ids.stft_hop_length.text)
+
+        window = self.ids.stft_window.text
+        if window == 'bartlett':
+            window = torch.bartlett_window(n_fft)
+        elif window == 'blackman':
+            window = torch.blackman_window(n_fft)
+        elif window == 'hamming':
+            window = torch.hamming_window(n_fft)
+        elif window == 'hann':
+            window = torch.hann_window(n_fft)
+        elif window == 'kaiser':
+            window = torch.kaiser_window(n_fft)
+        else:
+            window = None
+
         args = dict(
-            n_fft=int(self.ids.n_fft.text),
-            hop_length=int(self.ids.hop_length.text)
+            n_fft=n_fft,
+            hop_length=hop_length,
+            window=window
         )
         return args

utils/audio/silence_pyaudioanalysis.py
@@ -127,9 +127,9 @@ def silence_removal(
             if not high_t.any():
                 break
 
-            next_idx = idx + torch.where(high_t)[0][0].item() - 1
+            next_idx = idx + torch.where(high_t)[0][0].item()
             if next_idx - idx <= 3:
-                prob[list(range(idx, next_idx+1))] = 1
+                prob[list(range(idx, next_idx))] = 1
 
             low_t = prob[next_idx:] == 0
             if not low_t.any():
@@ -168,13 +168,23 @@ def silence_removal(
     General utility functions
 """
 def smooth_moving_avg(x, n_win=11):
-    if x.ndim == 1:
-        x = x[None, None]
-    elif x.ndim == 2:
-        x = x[:, None]
-    w = torch.ones((n_win, 1, 1), dtype=x.dtype)
+    if n_win < 3:
+        return x
 
-    return torch.conv1d(x, w, bias=None, padding='valid').squeeze() / n_win
+    if x.ndim == 1:
+        x = x[None]
+    elif x.ndim > 2:
+        raise ValueError('')
+
+    s = torch.hstack([
+        2*x[..., 0][:, None] - x.flip(-1)[..., -n_win:],
+        x,
+        2*x[..., -1][:, None] - x.flip(-1)[..., :n_win-1]
+    ])[:, None]
+    w = torch.ones((1, 1, n_win), dtype=s.dtype)/n_win
+
+    y = torch.conv1d(s, w, bias=None, padding='same').squeeze()
+    return y[..., n_win-1:-n_win]
 
 
 """