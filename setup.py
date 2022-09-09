4f847abccaa51b13d3e268091e536fa9472b40a1
Masaki-Ichimura <m.ichimura.lab@gmail.com>
2022-09-09 12:59:58 +0900

    Add package infomation

--- setup.py
+++ setup.py
@@ -4,22 +4,26 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 
-exclude_packages = ['kivy-sdk-packager', 'kivy-sdk-packager.win']
-packages = find_packages(include=['batools', 'batools.*'], exclude=exclude_packages)
-
 setup(
-    name="batools",
-    version="0.0.1",
-    description="BioacousticAnalysisTools",
-    author="Masaki-Ichimura",
-    packages=packages,
+    name='batools',
+    version='0.0.1',
+    description='python based bio-acoustic analysis tools',
+    author='Masaki-Ichimura',
+    author_email='m.ichimura@gmail.com',
+    url='https://github.com/Masaki-Ichimura/BioacousticsAnalysisTools',
     package_dir={'batools': 'batools'},
     package_data={
         'batools': list(map(str, (pathlib.Path.cwd()/'batools').glob(r'**/*.kv')))
     },
+    packages=find_packages(
+        include=['batools', 'batools.*'],
+        exclude=['kivy-sdk-packager', 'kivy-sdk-packager.win']
+    ),
     install_requires=install_requirements,
     entry_points={
-        'console_scripts': ["myapp=batools.app.run:main",]
+        'console_scripts': ['bagui=batools.app.run:main',]
     },
-    classifiers=['Programming Language :: Python :: 3.9']
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ]
 )
