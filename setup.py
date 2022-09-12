4c7347797463e4a65c9d26cf29e223a73998ab18
Masaki-Ichimura <m.ichimura.lab@gmail.com>
2022-09-12 13:25:32 +0900

    Add lines of installing garden.matpotlib

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import pathlib
+from os import system
 from setuptools import setup, find_packages
+from shutil import rmtree
+from site import getsitepackages
 
 with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
@@ -27,3 +30,12 @@ setup(
         'Programming Language :: Python :: 3.9',
     ]
 )
+
+# This lines are instead of `garden install matplotlib` or `kivy garden install matplotlib`
+# because there is no setup.py .
+# It's recommended manual install for garden.matplotlib if you can because of setuptools architecture.
+# If you choice manual install for garden.matplotlib, you nead modify part of build app script.
+garden_matplotlib_url = 'https://github.com/kivy-garden/garden.matplotlib'
+garden_matplotlib_dir = f'{getsitepackages()[0]}/kivy/garden/matplotlib'
+rmtree(garden_matplotlib_dir, ignore_errors=True)
+system(f'git clone {garden_matplotlib_url} {garden_matplotlib_dir}')
