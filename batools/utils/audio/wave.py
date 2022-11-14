497104923493b96620fa1f9f8671bddf35a848b4
Masaki-Ichimura <m.ichimura.lab@gmail.com>
2022-11-14 14:52:44 +0900

    Add normalization on saving audio signal

--- batools/utils/audio/wave.py
+++ batools/utils/audio/wave.py
@@ -1,4 +1,6 @@
+import torch
 import torchaudio
+from torchaudio.sox_effects import apply_effects_tensor
 
 
 def load_wave(file_name: str):
@@ -11,3 +13,11 @@ def metadata_wave(file_name: str):
         'num_frames', 'num_channels', 'sample_rate', 'bits_per_sample', 'encoding'
     ]
     return {param: getattr(metadata, param) for param in params}
+
+def save_wave(file_name: str, src: torch.Tensor, sample_rate: int, normalize: bool=True):
+    if normalize:
+        src, sample_rate = apply_effects_tensor(
+            src, sample_rate, [['gain', '-n']], channels_first=True
+        )
+
+    torchaudio.save(file_name, src, sample_rate, channels_first=True)
