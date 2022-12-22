c6b2f22a848adf370fc2582b61976f886937da9c
Masaki-Ichimura <m.ichimura.lab@gmail.coom>
2022-12-22 16:53:35 +0900

    Fix singular matrix error

--- batools/utils/audio/bss/ilrma.py
+++ batools/utils/audio/bss/ilrma.py
@@ -36,7 +36,7 @@ class ILRMA(tf_bss_model_base):
 
         # default to determined case
         if n_src is None:
-            n_src = X.shape[2]
+            n_src = n_chan
 
         # Only supports determined case
         assert n_chan == n_src, "There should be as many microphones as sources"
@@ -44,42 +44,37 @@ class ILRMA(tf_bss_model_base):
         # initialize the demixing matrices
         # The demixing matrix has the following dimensions (nfrequencies, nchannels, nsources),
         if W0 is None:
-            W = torch.tile(torch.eye(n_chan, n_src, dtype=dtype, device=device), (n_freq, 1, 1))
+            W = torch.eye(n_chan, n_src, dtype=dtype, device=device).tile(n_freq, 1, 1)
         else:
             W = W0.detach().clone()
 
         # initialize the nonnegative matrixes with random values
-        T = 0.1+0.9*torch.rand((n_src, n_freq, n_components), device=device)
-        V = 0.1+0.9*torch.rand((n_src, n_frames, n_components), device=device)
+        T = 0.1 + 0.9*torch.rand((n_src, n_freq, n_components), device=device)
+        V = 0.1 + 0.9*torch.rand((n_src, n_frames, n_components), device=device)
         R = torch.zeros((n_src, n_freq, n_frames), device=device)
-        I = torch.eye(n_src, device=device)
-        U = torch.zeros(
-            (n_freq, n_src, n_chan, n_chan), dtype=dtype, device=device
-        )
+        I = torch.eye(n_src, dtype=dtype, device=device)
+        # U = torch.zeros((n_freq, n_src, n_chan, n_chan), dtype=dtype, device=device)
         # product = torch.zeros((n_freq, n_chan, n_chan), dtype=dtype)
         lambda_aux = torch.zeros(n_src)
         eps = 1e-15
-        eyes = torch.tile(
-            torch.eye(n_chan, dtype=dtype, device=device), (n_freq, 1, 1)
-        )
+        eyes = I.tile(n_freq, 1, 1)
 
         # Things are more efficient when the frequencies are over the first axis
         Y = torch.zeros((n_freq, n_src, n_frames), dtype=dtype, device=device)
         X_original = X
-        X = X.permute(1, 2, 0).detach().clone()
+        X = X.permute(1, 2, 0)
 
-        # R = torch.matmul(T, V.swapaxes(1, 2))
-        torch.matmul(T, V.swapaxes(1, 2), out=R)
+        torch.matmul(T, V.mT, out=R)
 
         # Compute the demixed output
         def demix(Y, X, W):
-            Y[:, :, :] = torch.matmul(W, X)
+            torch.matmul(W, X, out=Y)
 
         demix(Y, X, W)
 
         # P.shape == R.shape == (n_src, n_freq, n_frames)
         P = (Y*Y.conj()).real.permute(1, 0, 2)
-        iR = (1/R).type(P.dtype)
+        iR = (1 / R).type(P.dtype)
 
         if T.dtype!=P.dtype or V.dtype!=P.dtype:
             T, V = T.type(P.dtype), V.type(P.dtype)
@@ -91,7 +86,7 @@ class ILRMA(tf_bss_model_base):
                 Y_t = Y.permute(2, 0, 1)
                 if proj_back:
                     z = projection_back(Y_t, X_original[:, :, 0])
-                    callback(Y_t * z[None, :, :].conj())
+                    callback(Y_t * z[None].conj())
                 else:
                     callback(Y_t)
 
@@ -100,47 +95,46 @@ class ILRMA(tf_bss_model_base):
                 ## NMF
                 ######
 
-                T[s, :, :] *= torch.sqrt(
-                    ((P[s, :, :] * iR[s, :, :]**2)@V[s, :, :]) / (iR[s, :, :]@V[s, :, :])
-                )
-                T[:] = T.clip(eps)
+                T[s, :, :] *= (((P[s] * iR[s]**2) @ V[s]) / (iR[s] @ V[s])).sqrt()
+                T[:] = T.clamp(min=eps)
 
-                R[s, :, :] = T[s, :, :]@V[s, :, :].T
-                R[:] = R.clip(eps)
+                R[s, :, :] = (T[s] @ V[s].T).clamp(min=eps)
 
-                iR[s, :, :] = 1 / R[s, :, :]
+                iR[s, :, :] = 1 / R[s]
 
-                V[s, :, :] *= torch.sqrt(
-                    ((P[s, :, :].T * iR[s, :, :].T**2)@T[s, :, :]) / (iR[s, :, :].T@T[s, :, :])
-                )
-                V[:] = V.clip(eps)
+                V[s, :, :] *= (((P[s].T * iR[s].T**2) @ T[s]) / (iR[s].T @ T[s])).sqrt()
+                V[:] = V.clamp(min=eps)
 
-                R[s, :, :] = T[s, :, :]@V[s, :, :].T
-                R[:] = R.clip(eps)
+                R[s, :, :] = (T[s] @ V[s].T).clamp(min=eps)
 
-                iR[s, :, :] = 1 / R[s, :, :]
+                iR[s, :, :] = 1 / R[s]
 
                 ## IVA
                 ######
 
                 # Compute Auxiliary Variable
                 # shape: (n_freq, n_chan, n_chan)
-                C = torch.matmul((X * iR[s, :, None, :]), X.swapaxes(1, 2).conj()) / n_frames
+                C = ((X * iR[s, :, None, :]) @ X.mT.conj()) / n_frames
 
-                WV = torch.matmul(W, C)
-                W[:, s, :] = torch.linalg.solve(WV, eyes[:, :, s]).conj()
+                WV = W @ C
+                try:
+                    W[:, s, :] = torch.linalg.solve(WV, eyes[..., s]).conj()
+                except torch.linalg.LinAlgError:
+                    import warnings
+
+                    warnings.warn("Singular matrix encountered, switching to pseudo-inverse")
+
+                    W[:, s, :] = (WV.pinverse() @ eyes[..., [s]])[..., 0]
 
                 # normalize
-                denom = torch.matmul(
-                    torch.matmul(W[:, None, s, :], C[:, :, :]), W[:, s, :, None].conj()
-                )
-                W[:, s, :] /= torch.sqrt(denom[:, :, 0])
+                denom = (W[:, None, s] @ C) @ W[:, s, :, None].conj()
+                W[:, s, :] /= denom[..., 0].sqrt()
 
             demix(Y, X, W)
             P[:] = (Y*Y.conj()).real.permute(1, 0, 2)
 
             for s in range(n_src):
-                lambda_aux[s] = 1 / torch.sqrt(P[s, :, :].mean())
+                lambda_aux[s] = 1 / P[s].mean().sqrt()
 
                 W[:, :, s] *= lambda_aux[s]
                 P[s, :, :] *= lambda_aux[s] ** 2
@@ -150,8 +144,8 @@ class ILRMA(tf_bss_model_base):
         Y = Y.permute(2, 0, 1).detach().clone()
 
         if proj_back:
-            z = projection_back(Y, X_original[:, :, 0])
-            Y *= z[None, :, :].conj()
+            z = projection_back(Y, X_original[..., 0])
+            Y *= z[None].conj()
 
         Ynkl = Y.permute(2, 1, 0)
 
