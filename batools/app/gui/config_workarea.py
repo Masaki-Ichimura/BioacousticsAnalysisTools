326ee5775c7a396d9e49fa42d0936e2c871a6796
Masaki-Ichimura <m.ichimura.lab@gmail.com>
2023-01-10 16:46:11 +0900

    Add ssl option

--- batools/app/gui/config_workarea.py
+++ batools/app/gui/config_workarea.py
@@ -13,6 +13,10 @@ class ConfigWorkingContainer(Container):
     def on_kv_post(self, *arg, **kwargs):
         self.ids.nav_drawer.set_state('open')
 
+        def set_stft_window(window):
+            self.ids.stft_window.set_text(self.ids.stft_window, window)
+            self.stft_window_menu.dismiss()
+
         stft_windows = ['bartlett', 'blackman', 'hamming', 'hann', 'kaiser']
         self.stft_window_menu = MDDropdownMenu(
             caller=self.ids.stft_window,
@@ -23,15 +27,53 @@ class ConfigWorkingContainer(Container):
                     viewclass='OneLineListItem',
                     text=window,
                     height=dp(54),
-                    on_release=lambda x=window: self.set_stft_window(x)
+                    on_release=lambda x=window: set_stft_window(x)
                 )
                 for window in stft_windows
             ]
         )
 
-    def set_stft_window(self, window):
-        self.ids.stft_window.set_text(self.ids.stft_window, window)
-        self.stft_window_menu.dismiss()
+        # def set_ssl_mic_array_type(mic_array_type):
+        #     self.ids.ssl_circ_direction.disabled = False if mic_array_type == '円形' else True
+
+        #     self.ids.ssl_mic_array_type.set_text(self.ids.ssl_mic_array_type, mic_array_type)
+        #     self.ssl_mic_array_type_menu.dismiss()
+
+        # ssl_mic_array_types = ['円形', '直線(未実装)']
+        # self.ssl_mic_array_type_menu = MDDropdownMenu(
+        #     caller=self.ids.ssl_mic_array_type,
+        #     hor_growth='right',
+        #     width_mult=4,
+        #     items=[
+        #         dict(
+        #             viewclass='OneLineListItem',
+        #             text=mic_array_type,
+        #             height=dp(54),
+        #             on_release=lambda x=mic_array_type: set_ssl_mic_array_type(x)
+        #         )
+        #         for mic_array_type in ssl_mic_array_types
+        #     ]
+        # )
+
+        def set_ssl_circ_direction(circ_direction):
+            self.ids.ssl_circ_direction.set_text(self.ids.ssl_circ_direction, circ_direction)
+            self.ssl_circ_direction_menu.dismiss()
+
+        ssl_circ_directions = ['左(反時計回り)', '右(時計回り)']
+        self.ssl_circ_direction_menu = MDDropdownMenu(
+            caller=self.ids.ssl_circ_direction,
+            hor_growth='right',
+            width_mult=4,
+            items=[
+                dict(
+                    viewclass='OneLineListItem',
+                    text=circ_direction,
+                    height=dp(54),
+                    on_release=lambda x=circ_direction: set_ssl_circ_direction(x)
+                )
+                for circ_direction in ssl_circ_directions
+            ]
+        )
 
     def get_stft_args(self):
         n_fft = int(self.ids.stft_n_fft.text)
@@ -53,6 +95,19 @@ class ConfigWorkingContainer(Container):
             window = None
 
         args = dict(n_fft=n_fft, hop_length=hop_length, window=window)
+
+        return args
+
+    def get_ssl_args(self):
+        args = dict(n_fft=int(self.ids.stft_n_fft.text))
+
+        if self.ids.ssl_circ_checkbox.active:
+            args['mic_array'] = 'circ'
+            if self.ids.ssl_circ_direction.text[0] == '左':
+                args['circ_direction'] = 'left'
+            else:
+                args['circ_direction'] = 'right'
+            args['circ_radius'] = int(self.ids.ssl_circ_radius.text)/100
 
         return args
 
